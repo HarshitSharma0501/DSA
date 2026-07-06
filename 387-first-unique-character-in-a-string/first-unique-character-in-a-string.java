b1d6806bbbf35c30c4046a641ad2b4980628bf69
HarshitSharma0501 <harshitsharma89629@gmail.com>
2026-07-06 09:49:02 +0530

    Time: 6 ms (84.84%) | Memory: 47.3 MB (6.33%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] arr =new int[26];
+        for(int i=0;i<s.length();i++){
+              char chi=s.charAt(i);
+              int idx=chi-'a';
+              arr[idx]++;
+        }
+         for(int i=0;i<s.length();i++){
+              char chi=s.charAt(i);
+              int idx=chi-'a';
+              if(arr[idx]==1){
+                return i;
+              }
+
+        }
+       
+        
+        
+        return -1;
+    }
+}
